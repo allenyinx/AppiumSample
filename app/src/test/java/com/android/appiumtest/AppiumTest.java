165cdb5bcef28a10bed062dbb01e53efedc20721
Allen Yin <allen.yin@sap.com>
2020-03-26 11:09:52 +0800

    modify for appium capacity

--- app/src/test/java/com/android/appiumtest/AppiumTest.java
+++ app/src/test/java/com/android/appiumtest/AppiumTest.java
@@ -26,9 +26,11 @@ public class AppiumTest {
     // id of Widget like EditText username & Password
     By userId = By.id(appPackageName + id + "email");
 
-    By password = By.id(appPackageName + id + "password");
+    By password = By.id("com.android.appiumtest:id/password");
+//    By password = By.id(appPackageName + id + "password");
     //id of Widget like Button
-    By login_Button = By.id(appPackageName + id + "email_sign_in_button");
+    By login_Button = By.id("com.android.appiumtest:id/email_sign_in_button");
+//    By login_Button = By.id(appPackageName + id + "email_sign_in_button");
 
 
     /**
@@ -39,17 +41,17 @@ public class AppiumTest {
     @Before
     public void setUp() throws MalformedURLException {
 
-        File appDir = new File("C:/Abhishek/AppiumTest/app/build/outputs/apk");
-        File app = new File(appDir, "app-debug.apk");
+        File appDir = new File("/Users/i306445/Documents/debug/");
+        File app = new File(appDir, "ApiDemos-debug.apk");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         //Appium Server Version Name check your version name of appium
-        capabilities.setCapability("appium-version", "1.4.16.1");
+        capabilities.setCapability("appium-version", "1.15.1");
         capabilities.setCapability("platformName", "Android");
         //Device Android Version
-        capabilities.setCapability("platformVersion", "6.0.1");
+        capabilities.setCapability("platformVersion", "10.0");
         //Device name displayed on Android Monitor
-        capabilities.setCapability("deviceName", "SM-A710F");
+        capabilities.setCapability("deviceName", "emulator-5554");
         // Package Name of Application
         capabilities.setCapability("appPackage", appPackageName);
         //Activity Name
@@ -63,9 +65,9 @@ public class AppiumTest {
     @Test
     public void loginTest() {
         driver.findElement(userId).sendKeys("test@xyz.com");
-        driver.navigate().back();
+//        driver.navigate().back();
         driver.findElement(password).sendKeys("test1234");
-        driver.navigate().back();
+//        driver.navigate().back();
         driver.findElement(login_Button).click();
         WebElement element = driver.findElement(By.id(appPackageName + id + "welcome_user"));
         assert element.getText().equals("test@xyz.com") : "Actual value is " + element.getText() + " did not match with expected value test@xyz.com";
